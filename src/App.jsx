ed377d2469614774f9577e3992a1e47307402b0d
Umang Raval <umangraval0365@gmail.com>
2024-09-29 22:08:23 +0530

    commit for nexa landing page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./sections/Header"
+
+const App = () => {
+  return (
+    <div>
+      <main className="overflow-hidden">
+        <Header/>
+      </main>
+    </div>
+  )
+}
+
+export default App
